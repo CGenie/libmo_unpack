4cc5ccdfad180030d652026d7b9b4ca25dc02cbd
Przemysław Kaminski <pk@intrepidus.pl>
2025-09-26 09:04:08 +0200

    MO_syslog in c file

--- src/logerrors.h
+++ src/logerrors.h
@@ -60,6 +60,6 @@
   void set_error_level(int val);
 
   /* NOTE: Your main function must define the MO_syslog routine */
-  inline void MO_syslog(int value, char* message, const function* const caller) {};
+  extern void MO_syslog(int value, char* message, const function* const caller);
 
 #endif
---
+++ src/mo_syslog.c
@@ -0,0 +1,10 @@
+//
+// Created by przemek on 26.09.2025.
+//
+
+#include "logerrors.h"
+
+inline void MO_syslog(int value, char* message, const function* const caller)
+{
+
+};
